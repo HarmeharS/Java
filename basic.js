34b3a40fff528c50dd6f260e343ce58b654ec55e
HarmeharS <154463081+HarmeharS@users.noreply.github.com>
2023-12-21 23:02:20 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,1 +1,13 @@
-console.log('HARMEHAR')
+let name = "Harmehar"
+let age = "15"
+let IsLoggedIn = 'False"
+
+/*DATA TYPES-:
+1.number => 2 to the power of 53 
+2.bigint 
+3.string => " "
+4. boolean => true/false
+5. null => standalone value
+6. undefined =>
+7. symbol => unique
+8. objects
